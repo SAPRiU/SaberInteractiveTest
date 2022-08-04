95289d9a55058bd6d87c5ad79a29f6460e71ce73
Сергей Пашов <spashov485@mso365.technolog.edu.ru>
2022-08-04 22:46:49 +0300

    Добавьте файлы проекта.

---
+++ SaberInteractiveTest/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SaberInteractiveTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class ListNode
+    {
+        public ListNode Previous;
+        public ListNode Next;
+        public ListNode Random;
+        public string Data;
+    }
+
+
+    class ListRandom
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(Stream s)
+        {
+
+        }
+
+        public void Deserialize(Stream s)
+        {
+        }
+    }
+
+}
